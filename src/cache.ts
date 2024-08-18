25fc1175b2461170441c7f07f8065dc8d6bae717
Ali Jahankah <aral2@yahoo.com>
2024-08-18 18:19:16 +0100

    feat: add a type for all-regions data

--- src/cache.ts
+++ src/cache.ts
@@ -1,15 +1,20 @@
-import { ApiResponse } from './types';
+import { AllRegionsData, ApiResponse } from './types';
 
 // Create an empty cache object with a proper type
 const cache: {
-  [key: string]: ApiResponse;
+  [key: string]: ApiResponse | AllRegionsData;
 } = {};
 
 // Create setter and getter for the cached data
-export const setCache = (region: string, data: ApiResponse): void => {
+export const setCache = (
+  region: string,
+  data: ApiResponse | AllRegionsData
+): void => {
   // Caching data for a specific region
   cache[region] = data;
 };
-export const getCache = (region: string): ApiResponse | undefined => {
+export const getCache = (
+  region: string
+): ApiResponse | AllRegionsData | undefined => {
   return cache[region];
 };
